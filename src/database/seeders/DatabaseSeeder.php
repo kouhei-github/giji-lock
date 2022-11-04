8c5e93364719c96150db0763fc3a192480da1fe7
nagamatsu-k <nagamatsu-k@dym.jp>
2022-11-04 14:49:33 +0900

    無駄な湖面アウトの削除 #2

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,12 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         $this->call([
             GroupSeeder::class,
             PostSeeder::class,
